f5173776fe6ce3a1f4838751a699644907d44f10
Gautam2401 <gautamiitk28@gmail.com>
2025-06-22 20:55:18 +0530

    Updated

--- src/Component/Body.jsx
+++ src/Component/Body.jsx
@@ -10,8 +10,8 @@ export default function BodyPage(){
             </div>
             
             <div className="item">
-                <div style={{padding:"20px", fontSize:"50px", fontFamily:"cursive", fontWeight:"600"}}>Take Control of Your Money</div>
-                <div style={{padding:"20px",fontSize:"20px", fontFamily:"Helvectica", fontWeight:"200"}}>Personal budgeting is the secret to financial freedom. Start your journey today.</div>
+                <div className="text" style={{padding:"20px", fontSize:"40px", fontFamily:"cursive", fontWeight:"600"}}>Take Control of Your Money</div>
+                <div className="text" style={{padding:"20px",fontSize:"20px", fontFamily:"Helvectica", fontWeight:"200"}}>Personal budgeting is the secret to financial freedom. Start your journey today.</div>
                 <div>
                     <button  className="getStartedBtn" onClick={() => navigate("/budgetpage")}>
                         Get Started
