6713fdf4d1646ee0db526756525db4812337a5be
Gautam2401 <gautamiitk28@gmail.com>
2025-06-22 19:04:30 +0530

    Update Body.jsx

--- src/Component/Body.jsx
+++ src/Component/Body.jsx
@@ -6,18 +6,18 @@ export default function BodyPage(){
     return(
         <div className="Maincontainer">
             <div className="item">
-                <div style={{marginLeft:"20px", fontSize:"50px", fontFamily:"cursive", fontWeight:"600"}}>Take Control of Your Money</div>
-                <div style={{marginLeft:"15px",fontSize:"20px", fontFamily:"Helvectica", fontWeight:"200"}}>Personal budgeting is the secret to financial freedom. Start your journey today.</div>
+                <img className="myImage" src={imagebody} />
+            </div>
+            
+            <div className="item">
+                <div style={{padding:"20px", fontSize:"50px", fontFamily:"cursive", fontWeight:"600"}}>Take Control of Your Money</div>
+                <div style={{padding:"20px",fontSize:"20px", fontFamily:"Helvectica", fontWeight:"200"}}>Personal budgeting is the secret to financial freedom. Start your journey today.</div>
                 <div>
                     <button  className="getStartedBtn" onClick={() => navigate("/budgetpage")}>
                         Get Started
                     </button>
                 </div>
                 </div>
-            
-            <div className="item">
-                <img className="myImage" src={imagebody} />
-            </div>
         </div>
     )
 }
